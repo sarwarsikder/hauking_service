cbc40bba0f26f38bc944d534930a2ac632377c85
Sarwar Sikder <sarwar.sikder@kaicomsol.com>
2022-07-24 22:40:28 +0600

    seedr added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,25 +22,25 @@ class DatabaseSeeder extends Seeder
         /***
          *  Seed admin fak data
          ***/
-//        User::factory()->create([
-//            'first_name' => fake()->firstName(),
-//            'last_name' => fake()->lastName(),
-//            'email' => 'admin@hauwk.com',
-//            'role' => 'admin',
-//            'email_verified_at' => now(),
-//            'status' => false,
-//            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-//            'remember_token' => Str::random(10),
-//        ]);
+        User::factory()->create([
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => 'admin@hauwk.com',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'status' => false,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
 
         /***
          *  Seed users fak data
          ***/
-        #User::factory(100)->create();
+        User::factory(100)->create();
         /***
          *  Seed services fak data
          ***/
-        #Service::factory(20)->create();
+        Service::factory(20)->create();
 
         /***
          *  Seed services fak data
