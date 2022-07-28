7072b132727c9128293ac6f540dea8925598c056
shawonmorshed <shawon@workspaceit.com>
2022-07-28 13:32:22 +0600

    service edit

---
+++ .history/app/Http/Controllers/backends/service/HaukingServiceController_20220728111008.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\backends\service;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ServiceRequest;
+use App\Service\HaukingService;
+use Illuminate\Http\Request;
+use App\Models\Service;
+use Response;
+use Auth;
+
+class HaukingServiceController extends Controller
+{
+    private array $data = [];
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $hauking_service = new HaukingService($request->toArray());
+            $this->data['haukings'] = $hauking_service->get();
+        } catch (\Exception $exception) {
+            return back()->withError($exception->getMessage())->withInput();
+        }
+
+        return view("backends.service.index", $this->data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("backends.service.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ServiceRequest $request)
+    {
+        try {
+         
+            $serviceObject = new Service();
+
+            $serviceObject->service_name = $request->service_name;
+            $serviceObject->subscription_type = $request->subscription_input_value;
+            $serviceObject->trial_period = $request->trial_period;
+            $serviceObject->hawkin_scale = json_encode($request->hawkin_scale);
+            $serviceObject->data_fields = $request->dataField_form_input_value;
+            $serviceObject->default_value_day = $age = json_encode(array("time"=>$request->default_value_day_time, "value"=>$request->default_value_day_value));
+            $serviceObject->default_value_night = json_encode(array("time"=>$request->default_value_night_time, "value"=>$request->default_value_night_value));
+            $serviceObject->default_value_booster = json_encode(array("time"=>$request->default_value_booster_time, "value"=>$request->default_value_booster_value));
+            $serviceObject->default_special_feq = $request->default_special_feq;
+            $serviceObject->created_by = Auth::user()->id;
+            $serviceObject->updated_by = Auth::user()->id;
+            
+            echo "<pre>";
+            print_r($request->file('service_image_url'));
+            die;
+
+            if ($request->file('service_image_url')) {
+                $file = $request->file('service_image_url');
+                $filename = date('YmdHi') . $file->getClientOriginalName();
+                $file->move(public_path('public/images/services'), $filename);
+                $userObject->service_image_url = $request['service_image_url'];
+            }
+            echo "<pre>";
+            print_r($serviceObject);
+            die;
+            if ($serviceObject->save()) {
+                return redirect(route('service-list'))->with('redirect-message', 'Service successfully added!');
+            } else {
+                return redirect()->back()->with('redirect-message', 'Something wrong!');
+            }
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ .history/database/seeders/DatabaseSeeder_20220728095724.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Coupon;
+use App\Models\Frequency;
+use App\Models\LanguageSettings;
+use App\Models\Service;
+use App\Models\Tax;
+use App\Models\User;
+use Database\Factories\AdminFactory;
+use Database\Factories\FrequencyFactory;
+use Database\Factories\TaxFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+       $this->call([
+           CountrySeeder::class
+       ]);
+       $this->call([
+           StateSeeder::class,
+       ]);
+       $this->call([
+           CitySeeder::class,
+       ]);
+
+       /***
+        *  Seed coupon fake data
+        ***/
+       Coupon::factory(20)->create();
+
+       /***
+        *  Seed services fake data
+        ***/
+       Frequency::factory(20)->create();
+       Tax::factory(20)->create();
+
+       /***
+        *  Seed admin fake data
+        ***/
+       LanguageSettings::factory(1)->create();
+
+
+        /***
+         *  Seed admin fake data
+         ***/
+        $this->call([
+            PaymentSettingsSeeder::class,
+        ]);
+
+       /***
+        *  Seed admin fak data
+        ***/
+       User::factory()->create([
+           'first_name' => fake()->firstName(),
+           'last_name' => fake()->lastName(),
+           'email' => 'admin@hauwk.com',
+           'role' => 'admin',
+           'email_verified_at' => now(),
+           'status' => false,
+           'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+           'remember_token' => Str::random(10),
+       ]);
+
+       /***
+        *  Seed users fake data
+        ***/
+       User::factory(100)->create();
+       /***
+        *  Seed services fake data
+        ***/
+       Service::factory(20)->create();
+
+    }
+}
